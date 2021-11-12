2d3a69965c6ab7e1ebee664cb8ea073224e4290b
alfonsusac <alfonsus.chendrawan@binus.ac.id>
2021-11-12 20:12:09 +0700

    Added Blade Template

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
-    //
+    public function viewLogin(){ return view( 'login' ); }
+    public function viewRegister(){ return view( 'register' ); }
+    public function viewHome(){ return view( 'home' ); }
+    public function viewManageBooks(){ return view( 'managebooks' ); }
+    public function viewBookDetails(){ return view( 'bookdetails' ); }
+    public function viewManageGenres(){ return view( 'managegenres' ); }
+    public function viewGenreDetails(){ return view( 'genredetails' ); }
+    public function viewManagerUsers(){ return view( 'managerusers' ); }
+    public function viewUserDetails(){ return view( 'userdetails' ); }
+    public function viewViewCart(){ return view( 'viewcart' ); }
+    public function viewEditCartItems(){ return view( 'editcartitems' ); }
+    public function viewViewTransactionHistory(){ return view( 'viewtransactionhistory' ); }
+    public function viewViewTransactionHistoryDetails(){ return view( 'viewtransactionhistorydetails' ); }
+    public function viewProfile(){ return view( 'profile' ); }
+    public function viewChangePassword(){ return view( 'changepassword' ); }
 }
